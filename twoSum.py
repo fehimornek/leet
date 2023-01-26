c508a0649aa8110c1b72b743798f739fa71931df
fehim <fehimornek@gmail.com>
2023-01-26 22:07:03 +0300

    o(n) complexity hash table solution

--- twoSum.py
+++ twoSum.py
@@ -4,3 +4,11 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+    # O(N) complexity using hash tables
+    def twoSum_optimized(self, nums: List[int], target: int) -> List[int]:
+        hash_set = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hash_set:
+                return [hash_set[target - nums[i]], i]
+            hash_set[nums[i]] = i
