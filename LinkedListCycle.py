bb4237fa2af78fb3d0c006159bf6ad6394e2964c
fehim <fehimornek@gmail.com>
2023-02-09 23:01:27 +0300

    Tortoise and the hare algorithm implementation

--- LinkedListCycle.py
+++ LinkedListCycle.py
@@ -6,9 +6,12 @@ class ListNode:
         self.val = x
         self.next = None
 
-nodes = [ListNode(i) for i in range(1000000)]
+
+nodes = [ListNode(i) for i in range(1_000)]
+
 for i in range(len(nodes) -1):
     nodes[i].next = nodes[i+1]
+
 nodes[999].next = nodes[200]
 
 """
@@ -47,8 +50,29 @@ def hasCycle_set(head):
 
 
 # TO-DO : tortoise and the hare algorithm
+"""
+explaination:
+we create two pointers called the tortoise and the hare. In every loop
+tortoise moves by one node and the hare moves by two nodes. If they overlap 
+it means that there is a loop in the linked list. If the hare gets to a node
+with None value it means there is no loop.
+"""
+def tortoise_hare(head):
+    tortoise = head
+    hare = head.next
+    while True:
+        if tortoise == hare:
+            return 1
+        if hare is None or hare.next is None:
+            return 0
+
+        tortoise = tortoise.next
+        hare = hare.next.next
 
 
+print(tortoise_hare(nodes[0]))
+
+"""
 start1 = timer()
 val1 = hasCycle_set(nodes[0])
 end1 = timer()
@@ -58,3 +82,4 @@ start2 = timer()
 val2 = hasCycle_dict(nodes[0])
 end2 = timer()
 print(timedelta(seconds=end2-start2))
+"""
